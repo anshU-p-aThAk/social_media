29afd0d6b4d3a2bc3f3803631123668a12f006d5
anshU-p-aThAk <surajshiv15ss@gmail.com>
2024-06-23 14:35:16 +0530

    useFollow hook added

--- frontend/src/components/common/RightPanel.jsx
+++ frontend/src/components/common/RightPanel.jsx
@@ -1,33 +1,37 @@
 import { Link } from "react-router-dom";
 import RightPanelSkeleton from "../skeletons/RightPanelSkeleton";
 import { USERS_FOR_RIGHT_PANEL } from "../../utils/db/dummy";
-import {useQuery} from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
+import useFollow from "../../hooks/useFollow";
+import LoadingSpinner from "./LoadingSpinner";
 
 const RightPanel = () => {
 
-	const{data: suggestedUsers, isLoading} = useQuery({
+	const { data: suggestedUsers, isLoading } = useQuery({
 		queryKey: ["suggestedUsers"],
-		queryFn: async() => {
+		queryFn: async () => {
 			try {
 
 				const res = await fetch("/api/users/suggested");
 				const data = await res.json();
 
-				if(!res.ok) {
+				if (!res.ok) {
 					throw new Error(data.message || "Something went wrong");
 				}
 				return data;
-				
+
 			} catch (error) {
 				throw new Error(error);
 			}
 		}
 	})
 
-	if(suggestedUsers?.length === 0) {
+	const { follow, isPending } = useFollow();
+
+	if (suggestedUsers?.length === 0) {
 		return <div className="md:w-64 w-0"></div>
 	}
-	
+
 
 	return (
 		<div className='hidden lg:block my-4 mx-2'>
@@ -66,9 +70,12 @@ const RightPanel = () => {
 								<div>
 									<button
 										className='btn bg-white text-black hover:bg-white hover:opacity-90 rounded-full btn-sm'
-										onClick={(e) => e.preventDefault()}
+										onClick={(e) => {
+											e.preventDefault();
+											follow(user._id);
+										}}
 									>
-										Follow
+										{isPending ? <LoadingSpinner size="sm" /> : "Follow"}
 									</button>
 								</div>
 							</Link>

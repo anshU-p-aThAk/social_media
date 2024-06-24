202dd0c1b71fb48d87d282dcf50ea84d6b8e189b
anshU-p-aThAk <surajshiv15ss@gmail.com>
2024-06-24 19:01:24 +0530

    like post mutation added

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -122,7 +122,10 @@ export const likeUnlikePost = async (req, res) => {
             //Unlike post
             await Post.updateOne({ _id: postId }, { $pull: { likes: userId } }) // remove user from likes array
             await User.updateOne({ _id: userId }, { $pull: { likedPosts: postId } })
-            res.status(200).json({ message: "Post unliked successfully" });
+
+            const updatedLikes = post.likes.filter((id) => id.toString() !== userId.toString())
+
+            res.status(200).json(updatedLikes);
         }
         else {
             //Like post
@@ -138,7 +141,9 @@ export const likeUnlikePost = async (req, res) => {
 
             await notification.save();
 
-            res.status(200).json({ message: "Post liked successfully" });
+            const updatedLikes = post.likes
+
+            res.status(200).json(updatedLikes);
         }
 
     } catch (error) {
